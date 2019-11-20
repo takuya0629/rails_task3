18c0fcdf6c57f86c40285335070dc9271cda8200
takuya hakiri <tyapi4@yahoo.co.jp>
2019-11-20 12:45:16 +0900

    [add]トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :pictures
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#toppage'
+  resources :sessions, only: %w!new create destroy!
+  
+  resources :users 
+  
+  resources :pictures do 
+    collection do 
+      post :confirm
+    end
+  end
 end
